34c231ab8c9cd7e04778bd90a4eeff3430c71092
loffis <loffe.knutsson@gmail.com>
2020-09-24 11:06:30 +0200

    fix indentation

--- src/App.js
+++ src/App.js
@@ -4,35 +4,34 @@ import './App.css';
 
 
 class Money extends React.Component {
-  constructor(props) {
+    constructor(props) {
     super(props);
     this.state = { counterValue: 10 };
     //this.increaseValue = this.increaseValue.bind(this);
-  }
+    }
 
-  increaseValue(increase) {
-      this.setState({
-          counterValue: this.state.counterValue + increase
-      });
-  }
+    increaseValue(increase) {
+        this.setState({
+            counterValue: this.state.counterValue + increase
+        });
+    }
 
-  doubleValue() {
-      this.setState({
-          counterValue: this.state.counterValue * 2
-      });
-  }
+    doubleValue() {
+        this.setState({
+            counterValue: this.state.counterValue * 2
+        });
+    }
 
-  render() {
-      return (
-          <div>
-              <h1>VALUE: {this.state.counterValue}</h1>
-              <button onClick={() => this.increaseValue((Math.floor(Math.random() * 10) ))}>+ random</button>
-              <button onClick={() => this.increaseValue(1)}>+1</button>
-              <button onClick={() => this.doubleValue()}>double</button>
-          </div>
-      );
-  }
+    render() {
+        return (
+            <div>
+                <h1>VALUE: {this.state.counterValue}</h1>
+                <button onClick={() => this.increaseValue((Math.floor(Math.random() * 10) ))}>+ random</button>
+                <button onClick={() => this.increaseValue(1)}>+1</button>
+                <button onClick={() => this.doubleValue()}>double</button>
+            </div>
+        );
+    }  
 }
-
 
 export default Money;
